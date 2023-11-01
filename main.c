b333290ad1c602486b671d41f2841382e93a916b
yoshigake <2261839209@qq.com>
2023-11-01 16:36:19 +0800

    show

--- main.c
+++ main.c
@@ -10,17 +10,18 @@ int main(void)
 	t=' ';
 	while(1)
 	{
-		OLED_ShowCHinese(0,0,0);//中
-		OLED_ShowCHinese(18,0,1);//景
-		OLED_ShowCHinese(36,0,2);//园
-		OLED_ShowCHinese(54,0,3);//电
-		OLED_ShowCHinese(72,0,4);//子
-		OLED_ShowCHinese(90,0,5);//科
-		OLED_ShowCHinese(108,0,6);//技
-		OLED_ShowString(0,2,"1.3' OLED TEST");
+		OLED_ShowCHinese(11,0,0);//电
+		OLED_ShowCHinese(29,0,1);//子
+		OLED_ShowCHinese(47,0,2);//科
+		OLED_ShowCHinese(65,0,3);//技
+		OLED_ShowCHinese(83,0,4);//大
+		OLED_ShowCHinese(101,0,5);//学
+		OLED_ShowCHinese(45,2,6);//田
+		OLED_ShowCHinese(65,2,7);//岩
+		OLED_ShowString(2,4,"0.96' OLED TEST");
 	//	OLED_ShowString(8,2,"ZHONGJINGYUAN");  
-	 	OLED_ShowString(20,4,"2023/11/01");  
-		OLED_ShowString(0,6,"ASCII:");  
+	 	OLED_ShowString(20,6,"2023/11/01");  
+		/*OLED_ShowString(0,6,"ASCII:");  
 		OLED_ShowString(63,6,"CODE:");  
 		OLED_ShowChar(48,6,t);//显示ASCII字符	   
 		t++;
@@ -30,7 +31,7 @@ int main(void)
 		delay_ms(500);
 		OLED_Clear();
 
-		delay_ms(500);
+		delay_ms(500);*/
 
 	}
 	
